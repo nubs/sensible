783f950330737a17df5880e3dd7d77172fba9da6
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-07-15 08:39:14 -0400

    Also force arrays for the strategies.

--- src/Strategy/EditorStrategy.php
+++ src/Strategy/EditorStrategy.php
@@ -13,15 +13,15 @@ class EditorStrategy extends ListStrategy implements StrategyInterface
     /**
      * Initialize the editor strategy.
      *
-     * @param string|string[] $editors The names to the potential editors.
-     *     The first command in the list that can be located will be used.
+     * @param string[] $editors The names to the potential editors.  The first
+     *     command in the list that can be located will be used.
      * @param \Nubs\Which\Locator $commandLocator The command locator.  This
      *     helps locate commands using PATH.
      * @param \Habitat\Environment\Environment $environment The environment
      *     variable wrapper.  Defaults to null, which just uses the built-in
      *     getenv.
      */
-    public function __construct($editors, CommandLocator $commandLocator, Environment $environment = null)
+    public function __construct(array $editors, CommandLocator $commandLocator, Environment $environment = null)
     {
         parent::__construct([new EnvironmentVariableStrategy('EDITOR', $environment), new CommandLocatorStrategy($editors, $commandLocator)]);
     }
--- src/Strategy/PagerStrategy.php
+++ src/Strategy/PagerStrategy.php
@@ -13,15 +13,15 @@ class PagerStrategy extends ListStrategy implements StrategyInterface
     /**
      * Initialize the pager strategy.
      *
-     * @param string|string[] $pagers The names to the potential pagers.
-     *     The first command in the list that can be located will be used.
+     * @param string[] $pagers The names of the potential pagers.  The first
+     *     command in the list that can be located will be used.
      * @param \Nubs\Which\Locator $commandLocator The command locator.  This
      *     helps locate commands using PATH.
      * @param \Habitat\Environment\Environment $environment The environment
      *     variable wrapper.  Defaults to null, which just uses the built-in
      *     getenv.
      */
-    public function __construct($pagers, CommandLocator $commandLocator, Environment $environment = null)
+    public function __construct(array $pagers, CommandLocator $commandLocator, Environment $environment = null)
     {
         parent::__construct([new EnvironmentVariableStrategy('PAGER', $environment), new CommandLocatorStrategy($pagers, $commandLocator)]);
     }
