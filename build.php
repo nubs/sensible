2623a3fc8f43a329f91d24dba815904ea41ec667
Spencer Rinehart <anubis@overthemonkey.com>
2017-07-24 14:31:00 -0500

    Update PHP_CodeSniffer to 3.0.

--- build.php
+++ build.php
@@ -1,12 +1,33 @@
 #!/usr/bin/env php
 <?php
 require 'vendor/autoload.php';
+require 'vendor/squizlabs/php_codesniffer/autoload.php';
 
+use PHP_CodeSniffer\Config as PHPCSConfig;
+use PHP_CodeSniffer\Files\FileList as PHPCSFileList;
+use PHP_CodeSniffer\Reporter as PHPCSReporter;
+use PHP_CodeSniffer\Ruleset as PHPCSRuleset;
+use PHP_CodeSniffer\Util\Tokens as PHPCSTokens;
 use PHPUnit\Util\Configuration as PHPUnitConfiguration;
 use PHPUnit\TextUI\TestRunner as PHPUnitTestRunner;
 
-$phpcsCLI = new PHP_CodeSniffer_CLI();
-$phpcsViolations = $phpcsCLI->process(['standard' => ['PSR1'], 'files' => ['src', 'tests', 'build.php']]);
+$phpcsConfig = new PHPCSConfig();
+$phpcsConfig->standards = ['PSR1'];
+$phpcsConfig->files = ['src', 'tests', 'build.php'];
+$tokens = new PHPCSTokens();
+$phpcsRuleset = new PHPCSRuleset($phpcsConfig);
+$phpcsReporter = new PHPCSReporter($phpcsConfig);
+
+$phpcsFileList = new PHPCSFileList($phpcsConfig, $phpcsRuleset);
+foreach ($phpcsFileList as $file) {
+    $file->process();
+    $phpcsReporter->cacheFileReport($file, $phpcsConfig);
+    $file->cleanUp();
+}
+
+$phpcsReporter->printReports();
+
+$phpcsViolations = $phpcsReporter->totalErrors + $phpcsReporter->totalWarnings;
 if ($phpcsViolations > 0) {
     exit(1);
 }
